d2f7fd4cafd9da09b91af3fc7c38e3cd73338a30
Brian Gorman <briangorman99@gmail.com>
2023-08-10 14:38:25 -0400

    add Expense model, remove example.js file

---
+++ models/Expense.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class Income extends Model {}
+
+Income.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(20, 2),
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.UUID,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    category_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: "expense_category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    freezeTableName: true,
+    modelName: "income",
+  }
+);
+
+export default Income;
+
+/**
+ * Housing
+ * Utilities
+ * Groceries / household essentials
+ * Vehicle
+ * Internet/cable/streaming
+ * Phone
+ * Debt payments
+ * Subscriptions / memberships
+ * Child care
+ * Health care
+ * Emergency
+ * Travel
+ * Dining
+ * Entertainment
+ */
