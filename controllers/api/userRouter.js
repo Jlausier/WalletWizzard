47dee3ceceaf47e88d245b555e8d88c66687a564
Brian Gorman <briangorman99@gmail.com>
2023-08-10 18:54:16 -0400

    add POST /api/users/ route to create a new user

--- controllers/api/userRouter.js
+++ controllers/api/userRouter.js
@@ -4,6 +4,32 @@ import { User } from "../../models/index.js";
 const router = express.Router();
 
 /**
+ * POST /api/users/
+ * @summary Create a new user and redirect to the overview page,
+ *   otherwise return an error message and code.
+ */
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res
+        .json({ user: userData, message: "Created user successfully" })
+        .redirect("/dashboard/overview");
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+/**
  * POST /api/users/login
  * @summary Login and redirect to the overview page,
  *   otherwise return an error message and code.
@@ -27,7 +53,7 @@ router.post("/login", async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.isSoftDeleted;
+      req.session.user_id = userData.id;
       req.session.logged_in = true;
       res
         .json({ user: userData, message: "Logged in successfully" })
