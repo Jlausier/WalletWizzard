10efcf8713177a0a4edd341e4ed6a99e70457281
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-08-10 23:07:56 -0400

    added expense category, goal category, and goal progression seeds and added them to the seed.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,14 +1,25 @@
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 
 import sequelize from "../config/connection.js";
-import { User, Income, Goal, Expense } from "../models/index.js";
+import {
+  User,
+  Income,
+  Goal,
+  Expense,
+  GoalCategory,
+  GoalProgression,
+  ExpenseCategory,
+} from "../models/index.js";
 
 const require = createRequire(import.meta.url); // construct the require method
 
 const userData = require("./userData.json");
 const incomeData = require("./incomeData.json");
 const goalData = require("./goalData.json");
+const goalCategoryData = require("./goalCategoryData.json");
+const goalProgressionData = require("./goalProgressionData.json");
 const expenseData = require("./expenseData.json");
+const expenseCategoryData = require("./expenseCategoryData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -28,7 +39,22 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  await GoalCategory.bulkCreate(goalCategoryData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await GoalProgression.bulkCreate(goalProgressionData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   await Expense.bulkCreate(expenseData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await ExpenseCategory.bulkCreate(expenseCategoryData, {
     individualHooks: true,
     returning: true,
   });
