cf0ceed6d1fbcc1ab99a27d435a1cc9ad6cd16ab
Brian Gorman <briangorman99@gmail.com>
2023-08-14 11:40:21 -0400

    update incomeChart.js to fetch data for chart

--- public/js/incomeChart.js
+++ public/js/incomeChart.js
@@ -1,32 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const data = [1500, 1800, 2200, 1900, 2500, 2800];
-  const labels = ["January", "February", "March", "April", "May", "June"];
+  function displayChart() {
+    const data = [1500, 1800, 2200, 1900, 2500, 2800];
+    const labels = ["January", "February", "March", "April", "May", "June"];
 
-  const ctx = document.getElementById("income-chart").getContext("2d");
+    const ctx = document.getElementById("income-chart").getContext("2d");
 
-  const incomeChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: "Income",
-          data: data,
-          borderColor: "#007bff",
-          fill: false,
-        },
-      ],
-    },
-    options: {
-      maintainAspectRatio: false,
-      scales: {
-        y: {
-          stacked: true,
-          grid: {
-            display: true,
+    const incomeChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Income",
+            data: data,
+            borderColor: "#007bff",
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            stacked: true,
+            grid: {
+              display: true,
+            },
           },
         },
       },
-    },
-  });
+    });
+  }
+
+  async function fetchIncomeData() {
+    /**
+     * @TODO Get config data from the page / handlebars data
+     */
+
+    const url = "/api/income";
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      displayChart();
+    } else {
+      // Load error message into chart container
+    }
+  }
+
+  fetchIncomeData();
 });
