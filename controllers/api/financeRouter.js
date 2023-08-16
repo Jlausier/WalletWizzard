8fd5ce1ebbc8b1e2683d53f8ac4afb401bc2a324
Brian Gorman <briangorman99@gmail.com>
2023-08-16 01:32:31 -0400

    add expense CRUD routes

--- controllers/api/financeRouter.js
+++ controllers/api/financeRouter.js
@@ -1,6 +1,63 @@
 import express from "express";
 import { Expense, Goal, GoalProgression, Income } from "../../models/index.js";
+import withAuth from "../../utils/auth.js";
 
 const router = express.Router();
+
+const createExpense = async (req, res) => {
+  try {
+    const expenseData = await Expense.create(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json(expenseData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+};
+
+router.post("/expense/:id", createExpense);
+
+const updateExpense = async (req, res) => {
+  try {
+    const expenseData = await Expense.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json(expenseData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+};
+
+router.put("/expense/:id", updateExpense);
+
+const deleteExpense = async (req, res) => {
+  try {
+    const expenseData = await Expense.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      res.status(404).json({ message: "Could not find expense with that ID." });
+      return;
+    }
+
+    res.status(200).json(expenseData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+};
+
+router.delete("/expense/:id", deleteExpense);
 
 export default router;
