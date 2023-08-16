82b0c60c7a982f40cf8a9d44de3a6b98ba443e0b
Brian Gorman <briangorman99@gmail.com>
2023-08-16 01:55:15 -0400

    connect finance router to api router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./userRouter.js";
+import financeRouter from "./financeRouter.js";
 
 const router = express.Router();
 
 router.use("/users", userRouter);
+router.use(financeRouter);
 
 export default router;
