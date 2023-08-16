74b38e2000629a322883f5636734c465ea55c323
Brian Gorman <briangorman99@gmail.com>
2023-08-16 01:49:13 -0400

    add goal progression CRUD routes, add income CRUD routes

--- controllers/api/financeRouter.js
+++ controllers/api/financeRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // ================================ EXPENSE ===================================
 
+// CREATE
 const createExpense = async (req, res) => {
   try {
     const expenseData = await Expense.create(req.body);
@@ -24,6 +25,7 @@ const createExpense = async (req, res) => {
 
 router.post("/expense", createExpense);
 
+// UPDATE
 const updateExpense = async (req, res) => {
   try {
     const expenseData = await Expense.update(req.body, {
@@ -46,6 +48,7 @@ const updateExpense = async (req, res) => {
 
 router.put("/expense/:id", updateExpense);
 
+// DELETE
 const deleteExpense = async (req, res) => {
   try {
     const expenseData = await Expense.destroy({
@@ -70,6 +73,7 @@ router.delete("/expense/:id", deleteExpense);
 
 // ================================ GOAL ======================================
 
+// CREATE
 const createGoal = async (req, res) => {
   try {
     const goalData = await Goal.create(req.body);
@@ -88,6 +92,7 @@ const createGoal = async (req, res) => {
 
 router.post("/goal", createGoal);
 
+// UPDATE
 const updateGoal = async (req, res) => {
   try {
     const goalData = await Goal.update(req.body, {
@@ -110,6 +115,7 @@ const updateGoal = async (req, res) => {
 
 router.put("/goal/:id", updateGoal);
 
+// DELETE
 const deleteGoal = async (req, res) => {
   try {
     const goalData = await Goal.destroy({
@@ -130,6 +136,144 @@ const deleteGoal = async (req, res) => {
   }
 };
 
-router.delete("/expense/:id", deleteGoal);
+router.delete("/goal/:id", deleteGoal);
+
+// ================================ GOAL PROGRESSION ==========================
+
+// CREATE
+const createGoalProgression = async (req, res) => {
+  try {
+    const goalProgressionData = await GoalProgression.create(req.body);
+
+    if (!goalProgressionData) {
+      res.status(400).json({ message: "Could not create goal instance." });
+      return;
+    }
+
+    res.status(201).json(goalProgressionData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+};
+
+router.post("/goal-progression", createGoalProgression);
+
+// UPDATE
+const updateGoalProgression = async (req, res) => {
+  try {
+    const goalProgressionData = await GoalProgression.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!goalProgressionData) {
+      res
+        .status(404)
+        .json({ message: "Could not find goal progression with that ID." });
+      return;
+    }
+
+    res.status(201).json(goalProgressionData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+};
+
+router.put("/goal-progression/:id", updateGoalProgression);
+
+// DELETE
+const deleteGoalProgression = async (req, res) => {
+  try {
+    const goalProgressionData = await GoalProgression.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!goalProgressionData) {
+      res
+        .status(404)
+        .json({ message: "Could not find goal progression with that ID." });
+      return;
+    }
+
+    res.status(201).json(goalProgressionData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+};
+
+router.delete("/goal-progression/:id", deleteGoalProgression);
+
+// ================================ INCOME ====================================
+
+// CREATE
+const createIncome = async (req, res) => {
+  try {
+    const incomeData = await Income.create(req.body);
+
+    if (!incomeData) {
+      res.status(400).json({ message: "Could not create income instance." });
+      return;
+    }
+
+    res.status(201).json(incomeData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+};
+
+router.post("/income", createIncome);
+
+// UPDATE
+const updateIncome = async (req, res) => {
+  try {
+    const incomeData = await Income.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!incomeData) {
+      res.status(404).json({ message: "Could not find income with that ID." });
+      return;
+    }
+
+    res.status(201).json(incomeData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+};
+
+router.put("/income/:id", updateIncome);
+
+// DELETE
+const deleteIncome = async (req, res) => {
+  try {
+    const incomeData = await Income.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!incomeData) {
+      res.status(404).json({ message: "Could not find income with that ID." });
+      return;
+    }
+
+    res.status(201).json(incomeData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+};
+
+router.delete("/income/:id", deleteIncome);
 
 export default router;
