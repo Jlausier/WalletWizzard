4a28d47d44118efaeb11fe62e3444a7c379a9c30
Brian Gorman <briangorman99@gmail.com>
2023-08-18 13:55:01 -0400

    remove test partial, add error page, move script and css from handlebars file to public folder

---
+++ public/js/dropdown.js
@@ -0,0 +1,32 @@
+/*Toggle dropdown list*/
+function toggleDD(myDropMenu) {
+  document.getElementById(myDropMenu).classList.toggle("invisible");
+}
+
+/*Filter dropdown options*/
+function filterDD(myDropMenu, myDropMenuSearch) {
+  var filter, a, i;
+  filter = document.getElementById(myDropMenuSearch).value.toUpperCase();
+  div = document.getElementById(myDropMenu);
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1)
+      a[i].style.display = "";
+    else a[i].style.display = "none";
+  }
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (
+    !event.target.matches(".drop-button") &&
+    !event.target.matches(".drop-search")
+  ) {
+    var dropdowns = document.getElementsByClassName("dropdownlist");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (!openDropdown.classList.contains("invisible"))
+        openDropdown.classList.add("invisible");
+    }
+  }
+};
